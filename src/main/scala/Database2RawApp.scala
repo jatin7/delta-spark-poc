f762db1dd9d454765ede2d50bccface2b4a8c863
javieriturra <javier.iturra.b@gmail.com>
2020-08-13 19:48:44 -0400

    Partition by RUT

--- src/main/scala/Database2RawApp.scala
+++ src/main/scala/Database2RawApp.scala
@@ -2,6 +2,8 @@
 import java.sql.Timestamp
 import java.time.format.DateTimeFormatter
 
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType, TimestampType, StringType}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object Database2RawApp extends App {
@@ -10,14 +12,23 @@ object Database2RawApp extends App {
 
   Seq("2020-08-10", "2020-08-11", "2020-08-12").foreach(x => extractAndLoad(Timestamp.valueOf(s"$x 00:00:00")))
 
+  val schema = StructType(Seq(
+    StructField("rut", StringType, nullable = false),
+    StructField("value", IntegerType, nullable = false),
+    StructField("created", TimestampType, nullable = false),
+    StructField("updated", TimestampType, nullable = true),
+  ))
+
   private def extractAndLoad(date: Timestamp): Unit = {
     val dateTs = DateTimeFormatter.ISO_DATE.format(date.toLocalDateTime)
     spark.read
       .option("header", "true")
-      .schema(Schemas.schema)
+      .schema(schema)
       .csv(s"${Paths.database}/$dateTs-snapshot")
       .where(s"created = '$dateTs' OR updated = '$dateTs'")
+      .withColumn("extractionDate", lit(dateTs))
       .write
+      .partitionBy("rut")
       .format("csv")
       .option("header", "true")
       .mode(SaveMode.Overwrite)
--- src/main/scala/Raw2DeltaApp.scala
+++ src/main/scala/Raw2DeltaApp.scala
@@ -2,25 +2,40 @@
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 
 object Raw2DeltaApp extends App {
 
   val spark = SparkSession.builder().master("local[*]").getOrCreate()
+
+  val schema = StructType(Seq(
+    StructField("rut", StringType, nullable = false),
+    StructField("value", IntegerType, nullable = false),
+    StructField("created", TimestampType, nullable = false),
+    StructField("updated", TimestampType, nullable = true),
+    StructField("extractionDate", TimestampType, nullable = true),
+  ))
 
   extractAndLoad()
 
   private def extractAndLoad(): Unit = {
     val rawDf: DataFrame = spark.read
       .option("header", "true")
-      .schema(Schemas.schema)
+      .schema(schema)
       .csv(Paths.raw)
-      .withColumn("changed", when(col("updated").isNotNull, col("updated")).otherwise(col("created")))
+      .withColumn("changed", when(
+        col("updated").isNotNull,
+        col("updated")
+      ).otherwise(
+        col("created")
+      ))
 
     val deltaDf = rawDf
       .withColumn("rank", rank.over(Window.partitionBy("rut").orderBy(col("changed").desc)))
       .where(col("rank").equalTo(1))
       .select("rut", "value", "created", "updated")
       .sort("rut")
+      .repartition(1)
 
     deltaDf.write
       .format("delta")
@@ -28,6 +43,8 @@ object Raw2DeltaApp extends App {
       .option("header", "true")
       .mode(SaveMode.Overwrite)
       .save(Paths.delta)
+
+    rawDf.show()
   }
 
 }
